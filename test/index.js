570fbe907ff5b62f931f47495a57ba71b5a93223
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2017-07-08 09:20:35 +0200

    Fix nested style tags breaking compilation

--- test/index.js
+++ test/index.js
@@ -105,6 +105,11 @@ test('works with external stylesheets (global only)', async t => {
   t.snapshot(code)
 })
 
+test('does not transpile nested style tags', async t => {
+  const { code } = await transform('./fixtures/nested-style-tags.js')
+  t.snapshot(code)
+})
+
 test('server rendering', t => {
   function App() {
     const color = 'green'
